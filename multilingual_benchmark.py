b9b5de3d829ba3d1dc725db08be383d397e28e0c
jon-tow <jonathantow1@gmail.com>
2024-03-05 03:24:08 +0000

    fix(bench): add check to avoid overwriting json results

--- multilingual_benchmark.py
+++ multilingual_benchmark.py
@@ -5,6 +5,7 @@ Usage: Run from project root directory with the following command:
 python multilingual_benchmark.py --model "stabilityai/stablelm-2-1_6b"
 ```
 """
+import os
 import argparse
 import subprocess
 
@@ -19,6 +20,10 @@ args = parser.parse_args()
 
 
 def main():
+    results_file = os.path.join("results", f"{args.model.replace('/', '_')}.json")
+    results_file = os.path.abspath(results_file)
+    os.makedirs(os.path.dirname(results_file), exist_ok=True)
+    assert not os.path.exists(results_file), f"{results_file} results already exists! Check the `results` dir!"
     subprocess.run(
         [
             "python",
@@ -28,11 +33,11 @@ def main():
             "--model",
             "hf-causal-experimental",
             "--model_args",
-            f"pretrained={args.model},trust_remote_code=True,add_special_tokens={args.add_special_tokens}",
+            f"pretrained={args.model},trust_remote_code=True,add_special_tokens={args.add_special_tokens},dtype={args.dtype}",
             "--batch_size",
             f"{args.batch_size}",
             "--output_path",
-            f"results/{args.model.replace('/', '_')}.json",
+            results_file,
             "--device",
             args.device,
             "--num_fewshot",
